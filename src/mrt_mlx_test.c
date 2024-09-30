e807c89a475560a7cd2f9a54c91e050912f37b17
Nathaniel Jackson <njackson@student.42Adel.org.au>
2024-09-30 16:51:05 +0930

    Closing the window now works

--- src/mrt_mlx_test.c
+++ src/mrt_mlx_test.c
@@ -2,12 +2,23 @@
 #include "minirt.h"
 #include <unistd.h>
 
+int window_closed(t_mlx *mlx)
+{
+	mlx_destroy_image(mlx->mlxptr, mlx->img.img);
+	if (DESTROY_DISPLAY)
+	{
+		mlx_destroy_display(mlx->mlxptr);
+		free(mlx->mlxptr);
+	}
+	exit(0);
+}
+
 int main(void)
 {
 	t_mlx	mlx;
 
 	get_mlx_dat(&mlx);
 	mlx_put_image_to_window(mlx.mlxptr, mlx.winptr, mlx.img.img, 0, 0);
-	sleep(20);
-	cleanup_mlx(&mlx);
+	mlx_hook(mlx.winptr, 17, 1L << 5, &window_closed, &mlx);
+	mlx_loop(mlx.mlxptr);
 }
