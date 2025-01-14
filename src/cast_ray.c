6840e62a67bb7ed4ab0a7606851430b186682603
Nicole Lehmeyer <nicole.lehmeyer@icloud.com>
2025-01-14 16:27:08 +1030

    Completed writing cast_ray_plane() && cast_ray_sphere()

--- src/cast_ray.c
+++ src/cast_ray.c
@@ -12,7 +12,6 @@
 
 #include "minirt.h"
 
-//  -- CAST_RAY() --
 t_hit	cast_ray(t_list *objs, t_ray ray)
 {
 	t_hit	hit;
@@ -33,19 +32,18 @@ t_hit	cast_ray(t_list *objs, t_ray ray)
 	}
 	return (hit);
 }
-//  -- CAST_RAY_SPHERE() --
 
 t_hit	cast_ray_sphere(t_obj *obj, t_ray ray)
 {
 	t_hit	hit;
 
 	hit.cam_dist = solve_quadratic(obj, ray);
-	if (hit.cam_dist == 0)
+	if (hit.cam_dist == 0 || hit.cam_dist > ray.max_dist)
 		return ((t_hit){.obj = NULL});
 	hit.obj = obj;
 	hit.normal = vec3_mult((vec3_add(hit.point, vec3_inverse(obj->pos))), (1 / obj->radius));
 	hit.point = vec3_add(ray.start, (vec3_mult(ray.dir, hit.cam_dist)));
-	return ((t_hit){.obj = NULL});
+	return (hit);
 }
 
 double	solve_quadratic(t_obj *obj, t_ray ray)
@@ -70,16 +68,7 @@ double	solve_quadratic(t_obj *obj, t_ray ray)
 		quad.t1 = 0;
 	return (quad.t1);
 }
-// MAX DISTANCE HANDLING
-// find closest point of object, not past [max distance]
-// params:
-//   t obj object (the type of obj) to check intersects
-//   angle direction of ray (normalised)
-//   max distance (double, infinity if no max dist)
-// returns: closest hit point, as t_hit datatype
 
-
-//  -- CAST_RAY_PLANE() --
 t_hit	cast_ray_plane(t_obj *obj, t_ray ray)
 {
 	t_hit	hit;
@@ -87,7 +76,7 @@ t_hit	cast_ray_plane(t_obj *obj, t_ray ray)
 	if (vec3_dot(ray.dir, obj->angle) == 0)
 		return ((t_hit){.obj = NULL});
 	hit.cam_dist = -(vec3_dot(vec3_add(ray.start, vec3_inverse(obj->pos)), obj->angle) / vec3_dot(ray.dir, obj->angle));
-	if (hit.cam_dist == 0)
+	if (hit.cam_dist <= 0 || hit.cam_dist > ray.max_dist)
 		return ((t_hit){.obj = NULL});
 	hit.obj = obj;
 	if (vec3_dot(obj->angle, ray.dir) > 0)
@@ -95,11 +84,5 @@ t_hit	cast_ray_plane(t_obj *obj, t_ray ray)
 	else
 		hit.normal = obj->angle;
 	hit.point = vec3_add(ray.start, (vec3_mult(ray.dir, hit.cam_dist)));
-	return ((t_hit){.obj = NULL});
+	return (hit);
 }
-// find closest point of object, not past [max distance]
-// params:
-//   t obj object (the type of obj) to check intersects
-//   angle direction of ray (normalised)
-//   max distance (double, infinity if no max dist)
-// returns: closest hit point, as t_hit datatype
