89d85e0c706aca76f325fb45b3e2e65aa0ba581c
Nicole Lehmeyer <nicole.lehmeyer@icloud.com>
2025-01-15 14:52:15 +1030

    Completed cast_ray.c functions, must norminette cast_ray.c

--- src/cast_ray.c
+++ src/cast_ray.c
@@ -32,20 +32,6 @@ t_hit	cast_ray(t_list *objs, t_ray ray)
 	}
 	return (hit);
 }
-//COMMENT
-// t_hit	cast_ray_sphere(t_obj *obj, t_ray ray)
-// {
-// 	t_hit	hit;
-
-// 	hit.cam_dist = solve_quadratic(obj, ray);
-// 	if (hit.cam_dist == 0 || hit.cam_dist > ray.max_dist)
-// 		return ((t_hit){.obj = NULL});
-// 	hit.obj = obj;
-// 	hit.normal = vec3_normalise(vec3_add(hit.point, vec3_inverse(obj->pos)));
-// 	hit.point = vec3_add(ray.start, (vec3_mult(ray.dir, hit.cam_dist)));
-// 	return (hit);
-// }
-//COMMENT
 
 t_hit	cast_ray_sphere(t_obj *obj, t_ray ray)
 {
@@ -55,10 +41,11 @@ t_hit	cast_ray_sphere(t_obj *obj, t_ray ray)
 	if (hit.cam_dist == 0 || hit.cam_dist > ray.max_dist)
 		return ((t_hit){.obj = NULL});
 	hit.obj = obj;
-	hit.normal = vec3_normalise(vec3_mult((vec3_add(hit.point, vec3_inverse(obj->pos))), (1 / obj->radius)));
 	hit.point = vec3_add(ray.start, (vec3_mult(ray.dir, hit.cam_dist)));
+	hit.normal = vec3_normalise(vec3_mult((vec3_add(hit.point, vec3_inverse(obj->pos))), (1 / obj->radius)));
 	return (hit);
 }
+
 double	solve_quadratic(t_obj *obj, t_ray ray)
 {
 	t_quad	quad;
@@ -94,35 +81,6 @@ double	solve_quadratic(t_obj *obj, t_ray ray)
 		quad.t1 = 0;
 	return (quad.t1);
 }
-
-// if t1 > t2
-// {
-// 	temp = t1
-// 	t1 = t2
-// 	t2 = temp;
-// }
-// double	solve_quadratic(t_obj *obj, t_ray ray)
-// {
-// 	t_quad	quad;
-
-// 	quad.a = vec3_dot(ray.dir, ray.dir);
-// 	quad.b = 2 * vec3_dot(vec3_add(ray.start, vec3_inverse(obj->pos)), ray.dir);
-// 	quad.c = vec3_dot(vec3_add(ray.start, vec3_inverse(obj->pos)),
-// 			vec3_add(ray.start, vec3_inverse(obj->pos))) - pow(obj->radius, 2);
-// 	quad.discrim = pow(quad.b, 2) - (4 * quad.a * quad.c);
-// 	if (quad.discrim > 0)
-// 	{
-// 		quad.t1 = (-quad.b + sqrt(quad.discrim)) / (2 * quad.a);
-// 		quad.t2 = (-quad.b - sqrt(quad.discrim)) / (2 * quad.a);
-// 	}
-// 	else if (quad.discrim == 0)
-// 		quad.t1 = -quad.b / (2 * quad.a);
-// 	if (quad.t2 > 0 && quad.t2 < quad.t1)
-// 		quad.t1 = quad.t2;
-// 	if (quad.t1 < 0)
-// 		quad.t1 = 0;
-// 	return (quad.t1);
-// }
 
 t_hit	cast_ray_plane(t_obj *obj, t_ray ray)
 {
